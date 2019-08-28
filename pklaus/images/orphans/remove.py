013ad72b827dd5255cba33c72aec4f731e18e65b
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2019-08-28 13:41:22 +0200

    pklaus.images.orphans.remove --raw to specify file extensions

--- pklaus/images/orphans/remove.py
+++ pklaus/images/orphans/remove.py
@@ -1,4 +1,12 @@
+RAW_EXTENSIONS = (
+    '.CR2', # for Canon
+    '.RAF', # for Fuji
+    '.NEF', # for Nikon
+    '.RW2', # for Lumix
+)
+
 def main():
+    global RAW_EXTENSIONS
     import argparse, re, sys, os
     from pklaus.files.removal.delete import comfort_delete
 
@@ -14,6 +22,8 @@ def main():
             help='Folder to move orphaned raw images to.')
     parser.add_argument('--tolerant', '-t', action='store_true',
             help='Accept JPEGs that just start with the name of the raw image as a match too.')
+    parser.add_argument('--raw', '-r',
+            help='Comma separated list of extensions considered a RAW image. Default: %s' % ','.join(RAW_EXTENSIONS))
     parser.add_argument('--quiet', '-q', action='store_true',
             help='Silence the less important output of this tool.')
     parser.add_argument('--dry', '-d', action='store_true',
@@ -22,12 +32,30 @@ def main():
             help='Folder to check for raw images. Defaults to the current working directory')
     args = parser.parse_args()
     verbose = not args.quiet
+    if args.raw:
+        RAW_EXTENSIONS = args.raw.split(',')
+
+    patterns = []
+    for ext in RAW_EXTENSIONS:
+        match = r''
+        for letter in ext:
+            if letter == '.':
+                match += r'\.'
+            elif letter in (str(num) for num in range(10)):
+                match += letter
+            else:
+                match += r'[' + letter.lower() + letter.upper() + r']'
+        patterns.append(match)
+    pattern = r'(.*)(' + r'|'.join(patterns) + r')$'
+    if verbose: print('Regular expression match pattern for RAW images:', pattern)
+
+
     raw_images, jpeg_images_bare_names = [], []
     all_files = list(os.listdir(args.folder))
     # sort files into raw and jpeg files
     for filename in all_files:
-        # The file name of raw image ends with .CR2 for Canon EOS cameras
-        if re.match(r'(.*)\.[cC][rR]2$', filename):
+        # The file name of raw image ends with
+        if re.match(pattern, filename):
             raw_images.append(filename)
         if re.match(r'(.*)\.[jJ][pP][eE]?[gG]$', filename):
             jpeg_images_bare_names.append(os.path.splitext(filename)[0])
@@ -44,7 +72,7 @@ def main():
         elif os.path.splitext(raw_image)[0] not in jpeg_images_bare_names:
             orphans.append(raw_image)
     if len(raw_images) + len(jpeg_images_bare_names) == 0:
-        if verbose: stderr("No images found. Are you sure you wanted to check '%s' for orphaned RAW images?" %
+        if verbose: stderr("No images found. No use checking '%s' for orphaned RAW images." %
                 (args.folder,))
         sys.exit(2)
     elif len(raw_images) == 0:
