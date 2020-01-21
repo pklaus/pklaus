c26e645fc65646598a5c5bce83adb85f14e22384
Philipp Klaus <philipp.l.klaus@web.de>
2020-01-21 16:56:34 +0100

    pklaus.audio.spectrum.plot fix Linux/new mpl incompat

--- pklaus/audio/spectrum/plot.py
+++ pklaus/audio/spectrum/plot.py
@@ -5,9 +5,10 @@ https://github.com/flothesof/LiveFFTPitchTracker/blob/master/LiveFFT.py
 
 import numpy as np
 
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 
 from PyQt5 import QtGui, QtCore, QtWidgets
 from PyQt5.QtCore import Qt
@@ -166,7 +167,7 @@ class MicrophoneRecorder(object):
 
 class MplFigure(object):
     def __init__(self, parent):
-        self.figure = plt.figure(facecolor='white')
+        self.figure = Figure(facecolor='white')
         self.canvas = FigureCanvas(self.figure)
         self.toolbar = NavigationToolbar(self.canvas, parent)
 
