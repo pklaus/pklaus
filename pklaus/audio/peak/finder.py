9ad0f303a1e3e7aa207f22807ab6ada94bb47b0f
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2020-01-22 16:32:55 +0100

    pklaus.audio.peak.finder 48kHz, 8192samples

--- pklaus/audio/peak/finder.py
+++ pklaus/audio/peak/finder.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 
 np.set_printoptions(suppress=True) # don't use scientific notation
 
-CHUNK = 4096 # number of data points to read at a time
-RATE = 44100 # time resolution of the recording device (Hz)
+CHUNK = 8192 # number of data points to read at a time
+RATE = 48000 # time resolution of the recording device (Hz)
 
 p=pyaudio.PyAudio() # start the PyAudio class
 stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
