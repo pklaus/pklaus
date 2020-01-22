043d0619ab6d988ef8d7e37599b30472c9942e9d
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2020-01-22 16:46:09 +0100

    pklaus.audio.peak.finder split into find_peak() and main()

--- pklaus/audio/peak/finder.py
+++ pklaus/audio/peak/finder.py
@@ -6,27 +6,32 @@ import pyaudio
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.set_printoptions(suppress=True) # don't use scientific notation
-
-CHUNK = 8192 # number of data points to read at a time
-RATE = 48000 # time resolution of the recording device (Hz)
-
-p=pyaudio.PyAudio() # start the PyAudio class
-stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
-              frames_per_buffer=CHUNK) #uses default input device
-
-# create a numpy array holding a single read of audio data
-while True:
-    data = np.frombuffer(stream.read(CHUNK), dtype=np.int16)
+def find_peak(data, fs=48000):
     data = data * np.hanning(len(data)) # smooth the FFT by windowing data
     fft = abs(np.fft.rfft(data))
     fft = fft[:int(len(fft)/2)] # keep only first half
-    freq = np.fft.rfftfreq(CHUNK, 1.0/RATE)
+    freq = np.fft.rfftfreq(len(data), 1.0/fs)
     freq = freq[:int(len(freq)/2)] # keep only first half
-    freqPeak = freq[np.where(fft==np.max(fft))[0][0]]+1
-    print("peak frequency: %d Hz"%freqPeak)
+    return freq[np.where(fft==np.max(fft))[0][0]]+1
 
-# close the stream gracefully
-stream.stop_stream()
-stream.close()
-p.terminate()
+def main():
+    CHUNK = 8192 # number of data points to read at a time
+    RATE = 48000 # time resolution of the recording device (Hz)
+    p=pyaudio.PyAudio() # start the PyAudio class
+    stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
+                  frames_per_buffer=CHUNK) #uses default input device
+    try:
+        while True:
+            data = np.frombuffer(stream.read(CHUNK), dtype=np.int16)
+            peak_f = find_peak(data, fs=RATE)
+            print("peak frequency: %d Hz" % peak_f)
+    except:
+        print()
+    finally:
+        # close the stream gracefully
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+if __name__ == "__main__":
+    main()
